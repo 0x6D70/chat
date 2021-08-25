b1bdd7eec0b2f60566032bc2e2334d909b7e2b15
0x6D70 <pfeifenberger.markus2003@gmail.com>
2021-08-25 13:45:57 +0200

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,14 @@
 echo "Deploying ..."
+
+# kill all instances of the chat server
+killall node
+
+rm -rf chat/
+
+git clone https://github.com/0x6D70/chat.git
+
+cd chat/
+
+npm install
+
+nohup sudo node server.js & disown
